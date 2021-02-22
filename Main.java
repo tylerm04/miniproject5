33d1267814e8d2a59e013ac8a90c7bcb963af3e1
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2021-02-22 14:23:27 +0000

    Finished but must add coments

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Welcome! What is your current speed?");
+    int speed = scan.nextInt();
+    speedometer(speed);
+
   }
+
+  public static boolean speedometer(int speed) {
+    if (speed < 50) {
+      System.out.println("Keep up your going too slow!");
+      return false;
+    } else {
+      Scanner scan = new Scanner(System.in);
+
+      System.out.println("What is your current speed?");
+      int currentSpeed = scan.nextInt();
+      return speedometer(currentSpeed);
+    }
+  }
+
 }
